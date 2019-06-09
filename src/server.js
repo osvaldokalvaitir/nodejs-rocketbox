89211d8a74276ca8128dbabe3c71df84e2f249d4
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2019-06-09 14:58:17 -0300

    Estrutura back-end

--- src/server.js
+++ src/server.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
+
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+io.on("connection", socket => {
+  socket.on("connectRoom", box => {
+    socket.join(box);
+  });
+});
 
 mongoose.connect(
   "mongodb+srv://usuario:usuario123@cluster0-2vq7f.mongodb.net/rocketbox?retryWrites=true&w=majority",
@@ -10,9 +23,16 @@ mongoose.connect(
   }
 );
 
+app.use((req, res, next) => {
+  req.io = io;
+
+  return next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
 
 app.use(require("./routes"));
 
-app.listen(3333);
+server.listen(3333);
